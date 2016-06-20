5775e95fe9eb7e5c5d832046eca61a11aa62a144
andrzej151 <andrzej.dabrowski94@gmail.com>
2016-06-20 12:48:50 +0200

    inne

---
+++ inne/GALA/final/firmy.sql
@@ -0,0 +1,85 @@
+DROP TABLE firmyo;
+CREATE TABLE firmyo(
+   id     INT  NOT NULL PRIMARY KEY 
+  ,firma  VARCHAR(110) NOT NULL
+  ,czesc  VARCHAR(12) NOT NULL
+  ,branza VARCHAR(113)
+  ,sektor VARCHAR(1) NOT NULL
+  ,obecna INT  NOT NULL
+);
+INSERT INTO firmyo(id,firma,czesc,branza,sektor,obecna) VALUES (17,'enxoo sp. z o.o.','I cz���',NULL,'0',0);
+INSERT INTO firmyo(id,firma,czesc,branza,sektor,obecna) VALUES (76,'Inter Cars S.A.','II cz���',NULL,'0',0);
+INSERT INTO firmyo(id,firma,czesc,branza,sektor,obecna) VALUES (2,'K�mmerer Polska','I / II cz���','automotive','A',1);
+INSERT INTO firmyo(id,firma,czesc,branza,sektor,obecna) VALUES (1,'ceneo','I cz�s�',NULL,'A',1);
+INSERT INTO firmyo(id,firma,czesc,branza,sektor,obecna) VALUES (3,'3M Wroc�aw Sp. z o.o.','I cz���','Produkcja','A',1);
+INSERT INTO firmyo(id,firma,czesc,branza,sektor,obecna) VALUES (4,'ALUMECO SERVICE Sp. z o.o.','I cz���','SSC, in�ynieria, produkcja','A',1);
+INSERT INTO firmyo(id,firma,czesc,branza,sektor,obecna) VALUES (33,'SoftServe Poland','I cz���','IT','A',1);
+INSERT INTO firmyo(id,firma,czesc,branza,sektor,obecna) VALUES (40,'Axxiome Polska Sp. z o. o.','II cz���',NULL,'A',0);
+INSERT INTO firmyo(id,firma,czesc,branza,sektor,obecna) VALUES (41,'Bank Zachodni WBK','II cz���','Bankowo�� i finanse','A',0);
+INSERT INTO firmyo(id,firma,czesc,branza,sektor,obecna) VALUES (42,'BPX S.A.','II cz���','IT/Konsulting/ERP/BI','A',0);
+INSERT INTO firmyo(id,firma,czesc,branza,sektor,obecna) VALUES (43,'Cargill Poland Sp. z o.o.','II cz���','Produkcja/rolnictwo','A',0);
+INSERT INTO firmyo(id,firma,czesc,branza,sektor,obecna) VALUES (6,'Astrum MH','I cz���','Elektroenergetyka','B',1);
+INSERT INTO firmyo(id,firma,czesc,branza,sektor,obecna) VALUES (7,'Atos IT Services Sp. z o.o.','I cz���','IT','B',1);
+INSERT INTO firmyo(id,firma,czesc,branza,sektor,obecna) VALUES (8,'BNY Mellon Sp. z o.o.','I cz���','Bankowo�� Inwestycyjna','B',1);
+INSERT INTO firmyo(id,firma,czesc,branza,sektor,obecna) VALUES (9,'Capgemini Software Solutions Center','I cz���','IT','B',1);
+INSERT INTO firmyo(id,firma,czesc,branza,sektor,obecna) VALUES (44,'Clearcode','II cz���','IT','B',0);
+INSERT INTO firmyo(id,firma,czesc,branza,sektor,obecna) VALUES (45,'CrazyCall','II cz���','IT','B',0);
+INSERT INTO firmyo(id,firma,czesc,branza,sektor,obecna) VALUES (46,'DataArt','II cz���','IT','B',0);
+INSERT INTO firmyo(id,firma,czesc,branza,sektor,obecna) VALUES (47,'Divante Sp. z o.o.','II cz���','IT/e-commerce','B',0);
+INSERT INTO firmyo(id,firma,czesc,branza,sektor,obecna) VALUES (10,'Centrum Us�ug Informatycznych we Wroc�awiu','I cz���','IT, Informatyczna','C',1);
+INSERT INTO firmyo(id,firma,czesc,branza,sektor,obecna) VALUES (11,'Centrum Zdrowia WroMedica','I cz���','biotechnologiczna, farmaceutyczna','C',1);
+INSERT INTO firmyo(id,firma,czesc,branza,sektor,obecna) VALUES (12,'Credit Suisse','I cz���','bankowo��','C',1);
+INSERT INTO firmyo(id,firma,czesc,branza,sektor,obecna) VALUES (13,'Danfoss Power Solutions sp. z o.o.','I cz���','Produkcja','C',1);
+INSERT INTO firmyo(id,firma,czesc,branza,sektor,obecna) VALUES (48,'Elica Group Polska Sp. z o.o.','II cz���','Produkcja AGD','C',0);
+INSERT INTO firmyo(id,firma,czesc,branza,sektor,obecna) VALUES (49,'Euro Bank S.A.','II cz���','Finanse/bankowo��','C',0);
+INSERT INTO firmyo(id,firma,czesc,branza,sektor,obecna) VALUES (50,'Faurecia','II cz���','motoryzacja, produkcja','C',0);
+INSERT INTO firmyo(id,firma,czesc,branza,sektor,obecna) VALUES (51,'FIBRAIN Sp. z o.o.','II cz���','Telekomunikacyjna','C',0);
+INSERT INTO firmyo(id,firma,czesc,branza,sektor,obecna) VALUES (14,'DK Marketing','I cz���','Call center','D',1);
+INSERT INTO firmyo(id,firma,czesc,branza,sektor,obecna) VALUES (15,'docFlow S.A.','I cz���','US�UGOWA - zarz�dzanie danymi i dokumentami','D',1);
+INSERT INTO firmyo(id,firma,czesc,branza,sektor,obecna) VALUES (16,'Dolno�l�ski Wojew�dzki Urz�d Pracy Centrum Informacji i Planowania Kariery Zawodowej','I cz���',NULL,'D',1);
+INSERT INTO firmyo(id,firma,czesc,branza,sektor,obecna) VALUES (34,'Tornos Technologies Poland Sp. z o.o.','I cz���','Obr�bka metalu','D',1);
+INSERT INTO firmyo(id,firma,czesc,branza,sektor,obecna) VALUES (38,'vm.pl','I cz���','IT','D',1);
+INSERT INTO firmyo(id,firma,czesc,branza,sektor,obecna) VALUES (52,'Fortaco Sp. z o.o.','II cz���','Produkcja','D',0);
+INSERT INTO firmyo(id,firma,czesc,branza,sektor,obecna) VALUES (53,'GRUPA INCO S.A. Zak�ad Produkcyjny w Borowie','II cz���','Produkcjna','D',0);
+INSERT INTO firmyo(id,firma,czesc,branza,sektor,obecna) VALUES (54,'Grupa Unity','II cz���','IT e-commerce','D',0);
+INSERT INTO firmyo(id,firma,czesc,branza,sektor,obecna) VALUES (55,'IBM Client Innovation Center','II cz���','IT','D',0);
+INSERT INTO firmyo(id,firma,czesc,branza,sektor,obecna) VALUES (56,'Ku�nia Ostr�w Wielkopolski Sp. z o.o.','II cz���','metalowa','D',0);
+INSERT INTO firmyo(id,firma,czesc,branza,sektor,obecna) VALUES (18,'EY DGN Poland','I cz���','Audyt, doradztwo podatkowo-finansowe, IT','E',1);
+INSERT INTO firmyo(id,firma,czesc,branza,sektor,obecna) VALUES (57,'LEONI Kabel Polska Sp. z o.o.','II cz���','Automotive','E',0);
+INSERT INTO firmyo(id,firma,czesc,branza,sektor,obecna) VALUES (19,'Hewlett Packard Enterprise','I cz���','Business service','F',1);
+INSERT INTO firmyo(id,firma,czesc,branza,sektor,obecna) VALUES (58,'LG Chem Polans sp.z o.o.','II cz���','Produkcja tworzyw sztucznych','F',0);
+INSERT INTO firmyo(id,firma,czesc,branza,sektor,obecna) VALUES (20,'IKEA','I cz���','Retail','G',1);
+INSERT INTO firmyo(id,firma,czesc,branza,sektor,obecna) VALUES (21,'Kruk S.A.','I cz���','Finansowa','G',1);
+INSERT INTO firmyo(id,firma,czesc,branza,sektor,obecna) VALUES (22,'LEDIKO Sp. z o.o.','I cz���','Projektowanie i produkcja o�wietlania typu LED','G',1);
+INSERT INTO firmyo(id,firma,czesc,branza,sektor,obecna) VALUES (23,'ManpowerGroup Sp. z o.o.','I cz���','Bran�a HR - doradztwo personalne, rekrutacja sta�a oraz tymczasowa','G',1);
+INSERT INTO firmyo(id,firma,czesc,branza,sektor,obecna) VALUES (59,'McKinsey Knowledge Center Poland Sp. z o.o.,','II cz���','Research and Analytics','G',0);
+INSERT INTO firmyo(id,firma,czesc,branza,sektor,obecna) VALUES (60,'Micro Solutions Sp. z o.o. Sp. k.','II cz���','IT','G',0);
+INSERT INTO firmyo(id,firma,czesc,branza,sektor,obecna) VALUES (61,'NetTeam Solutions S.A.','II cz���','IT','G',0);
+INSERT INTO firmyo(id,firma,czesc,branza,sektor,obecna) VALUES (62,'NG Engineering Sp. z o.o.','II cz���','Us�ugi in�ynieryjne w bran�y:motoryzacja, transport publiczny, przemys� ci��ki, oprzyrz�dowanie produkcyjne, AGD','G',0);
+INSERT INTO firmyo(id,firma,czesc,branza,sektor,obecna) VALUES (26,'PM Group','II cz���','Producent maszyn papierniczych','G',0);
+INSERT INTO firmyo(id,firma,czesc,branza,sektor,obecna) VALUES (5,'Amazon','I cz���','Logistyka','H',1);
+INSERT INTO firmyo(id,firma,czesc,branza,sektor,obecna) VALUES (24,'myPhone Sp. z o.o.','I cz���','Elektronika u�ytkowa','H',1);
+INSERT INTO firmyo(id,firma,czesc,branza,sektor,obecna) VALUES (25,'Nestle Polska S.A. oddzia� Purina','I cz���','Spo�ywcza','H',1);
+INSERT INTO firmyo(id,firma,czesc,branza,sektor,obecna) VALUES (27,'Pneumat System Sp. z o.o.','I cz���','pneumatyczna','H',1);
+INSERT INTO firmyo(id,firma,czesc,branza,sektor,obecna) VALUES (35,'TORRECID SA','I cz���','CERAMICZNA','H',1);
+INSERT INTO firmyo(id,firma,czesc,branza,sektor,obecna) VALUES (63,'NOKIA','II cz���','IT','H',0);
+INSERT INTO firmyo(id,firma,czesc,branza,sektor,obecna) VALUES (64,'PCC Rokita SA','II cz���','Chemia, In�ynieria, Produkcja','H',0);
+INSERT INTO firmyo(id,firma,czesc,branza,sektor,obecna) VALUES (65,'Perfecta Sp. z o.o. Sp.k.','II cz���','reklama','H',0);
+INSERT INTO firmyo(id,firma,czesc,branza,sektor,obecna) VALUES (66,'PGB Human Resources','II cz���','Agencja Rekrutacyjna','H',0);
+INSERT INTO firmyo(id,firma,czesc,branza,sektor,obecna) VALUES (72,'Sodexo Polska','II cz���','Facility Management / techniczna obs�uga nieruchomo�ci','H',0);
+INSERT INTO firmyo(id,firma,czesc,branza,sektor,obecna) VALUES (28,'QMAF Sp. z o.o.','I cz���','In�ynieria jako�ci, Zarz�dzanie produkcj�','I',1);
+INSERT INTO firmyo(id,firma,czesc,branza,sektor,obecna) VALUES (29,'RST IT','I cz���','IT - Rozw�j oprogramowania','I',1);
+INSERT INTO firmyo(id,firma,czesc,branza,sektor,obecna) VALUES (30,'RW Swiss Automation sp. z o.o.','I cz���','Automotive','I',1);
+INSERT INTO firmyo(id,firma,czesc,branza,sektor,obecna) VALUES (31,'Ryanair / Travel Labs Poland Sp. z o.o.','I cz���','IT','I',1);
+INSERT INTO firmyo(id,firma,czesc,branza,sektor,obecna) VALUES (36,'Trenkwalder','I cz���','Agencja Pracy','I',1);
+INSERT INTO firmyo(id,firma,czesc,branza,sektor,obecna) VALUES (37,'UNIBEP S.A.','I cz���','Budownictwo','I',1);
+INSERT INTO firmyo(id,firma,czesc,branza,sektor,obecna) VALUES (67,'PROCOM SYSTEM S.A.','II cz���','Automatyka','I',0);
+INSERT INTO firmyo(id,firma,czesc,branza,sektor,obecna) VALUES (68,'PwC','II cz���','Consulting','I',0);
+INSERT INTO firmyo(id,firma,czesc,branza,sektor,obecna) VALUES (69,'REC a GlobalLogic Company','II cz���','IT','I',0);
+INSERT INTO firmyo(id,firma,czesc,branza,sektor,obecna) VALUES (70,'Robert Bosch Sp. z o.o.','II cz���','Automotive','I',0);
+INSERT INTO firmyo(id,firma,czesc,branza,sektor,obecna) VALUES (71,'Schneider Electric Energy Poland Sp. z o.o. Zak�ad Automatyki i System�w Elektroenergetycznych w �wiebodzicach','II cz���','Energetyka','I',0);
+INSERT INTO firmyo(id,firma,czesc,branza,sektor,obecna) VALUES (73,'Stelweld Sp. z o.o.','II cz���','Produkcja','I',0);
+INSERT INTO firmyo(id,firma,czesc,branza,sektor,obecna) VALUES (32,'Simoldes Plasticos Polska Sp. z o.o.','I cz���','Produkcja, automotiv','J',1);
+INSERT INTO firmyo(id,firma,czesc,branza,sektor,obecna) VALUES (74,'Strabag Infrastruktura Po�udnie Sp.zo.o.','II cz���','infrastruktura drogowa','J',0);
+INSERT INTO firmyo(id,firma,czesc,branza,sektor,obecna) VALUES (39,'WHITE HATS','I cz���','IT','K',1);
+INSERT INTO firmyo(id,firma,czesc,branza,sektor,obecna) VALUES (75,'Wroc�awski Park Technologiczny SA','II cz���',NULL,'K',0);
